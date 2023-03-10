9c4a115d3f2aba43fa25f8d341461f3dfb442c26
Muhammad Rafi <muhammadrafi2545@gmail.com>
2023-03-10 17:55:08 +0700

    tugas 15 js

---
+++ tugas15.js
@@ -0,0 +1,10 @@
+var ab = Math.pow(8,2)
+var bc = Math.pow(6,2)
+var ac = ab + bc;
+console.log("Nilai BC : 6");
+console.log("Nilai AB : 8");
+console.log(ab);
+console.log(bc);
+console.log(ac);
+console.log(Math.sqrt(ac));
+console.log(`panjang sisi AC pada segitiga siku-siku adalah : 10 cm`);
